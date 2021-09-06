44cae91eb4a122b35d1970c762aaf1f463e1daa8
naonao-program <gf19a012@oecu.jp>
2021-09-06 22:58:24 +0900

    ログアウト機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root to: 'lessons#index'
   resources :lessons, only: [:index] do
     resources :tasks, only: [:index, :show], shallow: true
